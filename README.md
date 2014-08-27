a762d06ab72b3e306873495cbd0307aab4d3674f
mdimercurio <matthieu.dimercurio@gmail.com>
2014-08-26 22:57:00 -0400

    Functions added and Classification tree

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Kaggle Challenge: Titanic - Machine Learning from a Disaster
 
 [Link to the Kaggle Competition's page](http://www.kaggle.com/c/titanic-gettingStarted)
 
+From Trevor's blog:
+
+[Getting started with R](http://trevorstephens.com/post/72916401642/titanic-getting-started-with-r)
+
+[Feature exploration](http://trevorstephens.com/post/72920580937/titanic-getting-started-with-r-part-2-the)
--- main.R
+++ main.R
@@ -1,6 +1,12 @@
 library(data.table)
 
+library(rpart)
+library(rattle)
+library(rpart.plot)
+library(RColorBrewer)
+
 train <- fread('./data/train.csv')
+test <- fread('./data/test.csv')
 
 # Dataset visualization =========================
 
@@ -25,21 +31,57 @@ train[,Embarked:=as.factor(Embarked)]
 #TODO create Fare bin
 #TODO extract info from Cabin
 
+# Compute the error rate on the training set
+computeError <- function(train, predictions) {
+  t <- table(predictions, train$Survived, useNA="ifany")
+  
+  errorRate <- (sum(t) - t[1,1] - t[2,2]) / sum(t)
+  return(errorRate)  
+}
+# Create a file ready for submission on Kaggles website
+# and a file describing the model used
+createSubmissionFile <- function(train, test, predictions, model) {
+  
+  SubmissionFileName <- paste('./output/Titanic Submission ', gsub('[[:punct:]]', '-', Sys.time()), '.csv', sep='')
+  DescriptionFileName <- paste ('./output/Titanic Submission Description ', gsub('[[:punct:]]', '-', Sys.time()), '.txt', sep='')
+  
+  # Replace NAs predictions by 0
+  numNAs <- sum(is.na(predictions))
+  predictions[is.na(predictions)] <- 0
+    
+  submit <- data.frame(PassengerId = test$PassengerId, Survived = predictions)
+  
+  write.csv(submit, file = SubmissionFileName, row.names = FALSE)
+  
+  # Create a description file, including the model description and the number of NAs 
+  write(as.character(model$call),file=DescriptionFileName,append=TRUE)
+  write(paste('Number of NAs: ', numNAs),file=DescriptionFileName,append=TRUE)
+  # TODO add error on training set
+}
+
 # Modeling ======================================
 
 # Logistic Regression
 #TODO Look into http://data.princeton.edu/R/glms.html
 logRegModel1 <- glm( Survived ~ 
-       +  Pclass + Sex + Age + Embarked , family = binomial, data=train)
-
-logRegModel2 <- glm( Survived ~ 
-                       +  PclassFact + Sex + Age + Embarked , family = binomial, data=train)
+       +  Pclass + Sex + Age + SibSp + Parch + Fare + Embarked, family = binomial, data=train)
 
 trainPredict <- predict.glm(logRegModel1, newdata=train, type="response")
-trainPredict <- ifelse(trainPredict > 0.5, 1, 0)
+trainPredictBool <- ifelse(trainPredict > 0.6, 1, 0)
 
-t <- table(trainPredict, train$Survived, useNA="ifany")
+testPredict <- predict.glm(logRegModel1, newdata=test, type="response")
+testPredictBool <- ifelse(testPredict > 0.5, 1, 0)
 
-errorRate <- (sum(t) - t[1,1] - t[2,2]) / sum(t)
+createSubmissionFile(train, test, testPredictBool, logRegModel1)
+ 
+# Decision Tree
 
-errorRate
+fit <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked, data=train, method="class")
+
+#plot(fit)
+#text(fit)
+#fancyRpartPlot(fit)
+
+Prediction <- predict(fit, test, type = "class")
+
+createSubmissionFile(train, test, Prediction, fit)
